c47035ec06952d65c4d3773807e603b460c5700b
jogonzro <jmgonzalezro@gmail.com>
2022-02-11 19:36:17 +0100

    tracking

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import os
+import hydra
+from hydra import utils
+
 
 DF_ACCIDENT=os.environ['ACCIDENT_ROUTE']
 DF_ROUTE=os.environ['CASUALITY_ROUTE']
 DF_VEHICLE=os.environ['VEHICLE_ROUTE']
+
+
